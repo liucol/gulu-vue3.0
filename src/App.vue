549d8c4e8c9167e1cd9d19e6512bb2529fc6775e
yangliu <1223181534@qq.com>
2020-09-02 15:31:42 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <pearl />
+</div>
+</template>
+
+<script>
+import pearl from './components/pearl.vue'
+
+export default {
+    name: 'App',
+    components: {
+        pearl
+    }
+}
+</script>
